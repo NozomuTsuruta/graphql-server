ef8e0de00865f5e9b844732b733f865a0c53a039
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-24 17:09:35 +0900

    元に戻した

--- server/sequelize/index.ts
+++ server/sequelize/index.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 import path from "path";
 import { Sequelize } from "sequelize";
 
-const env = process.env.NODE_ENV || "production";
+const env = process.env.NODE_ENV || "develop";
 
 /** sequelizeの設定ファイル */
 const config = require(path.join(__dirname, "./config.json"))[env];
--- server/sequelize/models/index.js
+++ server/sequelize/models/index.js
@@ -1,19 +1,23 @@
 "use strict";
 
-require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "develop";
+const config = require(__dirname + "/../config.json")[env];
 const db = {};
 
 let sequelize;
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: "postgres" });
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, null, {
-    dialect: "postgres",
-  });
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 fs.readdirSync(__dirname)
