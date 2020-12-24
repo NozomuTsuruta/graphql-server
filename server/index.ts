e02cca9eaf252a9bea3f2b100afd056731015657
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-24 13:41:36 +0900

    protocol追加

--- server/index.ts
+++ server/index.ts
@@ -30,8 +30,6 @@ const get_current_user = async (req: Request) => {
 
 /** apollo-serverの作成 */
 const apollo_server = new ApolloServer({
-  introspection: true,
-  playground: true,
   typeDefs: schema,
   resolvers: resolvers,
   context: async ({ req }) => {
