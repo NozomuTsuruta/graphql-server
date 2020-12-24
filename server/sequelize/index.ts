6937228b66272c72dc96670d383cefe8de10b705
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-24 17:51:59 +0900

    sequelizeのバグ修正

--- server/sequelize/index.ts
+++ server/sequelize/index.ts
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 import path from "path";
 import { Sequelize } from "sequelize";
 
-const env = process.env.NODE_ENV || "develop";
+const env = process.env.NODE_ENV || "development";
 
 /** sequelizeの設定ファイル */
 const config = require(path.join(__dirname, "./config.json"))[env];
